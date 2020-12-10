fa5c5dec891b4a37616931897bff83de0a47563f
xliang <171639931@qq.com>
2020-12-10 14:34:45 +0800

    update

--- startup.py
+++ startup.py
@@ -1,4 +1,6 @@
 import os
-cmd = "robot --listener allure_robotframework;./allure_xml -d ./report ./testsuite"
+current_path=os.path.abspath(__file__)
+project_root_path=os.path.abspath(os.path.dirname(current_path)+os.path.sep+'.')
+cmd = "robot -P %s --listener allure_robotframework;./allure_xml -d ./report ./testsuite" %project_root_path
 os.system(cmd)
 
